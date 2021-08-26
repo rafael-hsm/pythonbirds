958f707eee2e9d76724fc6c6a4ed3484df293754
rafael-hsm <rafameireles2011@gmail.com>
2021-08-26 08:55:30 -0300

    Criado atributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=31):
+    def __init__(self, *filhos, nome=None, idade=31):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     """
     self.nome determina o nome o objeto self, é um atributo. 
     Quando não temos o self, o nome é interpretado como parâmetro.
@@ -11,12 +12,11 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa("Ana")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Rafael'
-    print(p.nome)
-    print(p.idade)
-
+    luciano = Pessoa(nome="Luciano")
+    rafael = Pessoa(luciano, nome="Rafael")
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    for filho in rafael.filhos:
+        print(filho.nome)

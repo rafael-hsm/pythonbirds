f115316dbc1afde5f0dc106a7d9e05ded58f60c4
rafael-hsm <rafameireles2011@gmail.com>
2021-08-29 10:46:47 -0300

    Atualizando alterações das últimas 6 aulas.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,7 +10,7 @@ class Pessoa:
     Quando não temos o self, o nome é interpretado como parâmetro.
     """
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -21,9 +21,19 @@ class Pessoa:
         return f'{cls} - olhos {cls.olhos}'
 
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe} Aperto de mão!'
+
+
+class Mutante(Pessoa):
+    olhos = 3
+
+
 if __name__ == '__main__':
-    dexter = Pessoa(nome="Dexter")
-    rafael = Pessoa(dexter, nome="Rafael")
+    dexter = Mutante(nome="Dexter")
+    rafael = Homem(dexter, nome="Rafael")
     print(Pessoa.cumprimentar(dexter))
     print(id(dexter))
     print(dexter.cumprimentar())
@@ -36,10 +46,17 @@ if __name__ == '__main__':
     del dexter.olhos
     print(rafael.__dict__)
     print(dexter.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(dexter.olhos)
     print(rafael.olhos)
     print(id(rafael.olhos), id(dexter.olhos), id(rafael.olhos))
     print(Pessoa.metodo_estatico(), dexter.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), dexter.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(rafael, Pessoa))
+    print(isinstance(rafael, Homem))
+    print(dexter.olhos)
+    print(dexter.cumprimentar())
+    print(rafael.cumprimentar())
